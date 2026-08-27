41934c397d5ffcca2ef589b09cd3c8ff65d87435
ExarKun28 <jedmendoza101@gmail.com>
2026-08-27 16:10:33 +0800

    Update chairData.js

--- src/chairData.js
+++ src/chairData.js
@@ -1,5 +1,5 @@
 export const imageUrls = {
-  logo: 'https://nanami-massage-chair-images.s3.ap-southeast-2.amazonaws.com/logo.png',
-  hero: 'https://nanami-massage-chair-images.s3.ap-southeast-2.amazonaws.com/chairs.jpg',
-  blackchair: 'https://nanami-massage-chair-images.s3.ap-southeast-2.amazonaws.com/blackchair.jpg'
+  logo: '/logo.png',
+  hero: '/chairs.jpg',
+  blackchair: '/elite.png'
 };
